c27e0d581b201fcb12620363480fbde4d7109f8a
NomanLatif1723 <nomanlatif1723@gmail.com>
2023-07-14 20:50:32 +0500

    timer js

--- assets/theme.js
+++ assets/theme.js
@@ -1,24 +1,7 @@
-// const startTime = 1;
-// let time = startTime * 60 ;
-// const timerEl = document.querySelector('.main__timer');
-
-
-// let timer = setInterval(updateCountDown,1000);
-// function updateCountDown(){
-//   const minutes = Math.floor(time / 60);
-//   let seconds = time % 60;
-//   seconds = seconds < 10 ? '0' + seconds : seconds;
-//   timerEl.innerHTML = `${minutes} : ${seconds}`;
-//   if ( minutes == 0 && seconds < 1){
-//     clearInterval(timer);
-//     // document.querySelector('.timer_text_icon').style.display = 'none';
-//     // document.querySelector('.timer_completion_msg').style.display = 'block';
-    
-//   } else {
-//   time--;
-//   }
-// }
-
+const timerEl = document.querySelector('.main__timer');
+const hours = timerEl.getAttribute('data-hour');
+const minutes = timerEl.getAttribute('data-minutes');
+if(timerEl){
 function startTimer(hours, minutes) {
     // Convert hours and minutes to milliseconds
     var totalMilliseconds = (hours * 3600000) + (minutes * 60000);
@@ -34,9 +17,7 @@ function startTimer(hours, minutes) {
       var remainingHours = Math.floor(totalMilliseconds / 3600000);
       var remainingMinutes = Math.floor((totalMilliseconds % 3600000) / 60000);
       var remainingSeconds = Math.floor((totalMilliseconds % 60000) / 1000);
-  
-      // Output the remaining time
-      const timerEl = document.querySelector('.main__timer');
+      
       timerEl.innerHTML = `${remainingHours} : ${remainingMinutes} : ${remainingSeconds}`;
   
       // Reduce remaining time by 1 second
@@ -45,14 +26,9 @@ function startTimer(hours, minutes) {
       // Check if the timer has ended
       if (totalMilliseconds <= 0) {
         clearInterval(timerInterval);
-        console.log("Timer has ended");
+        timerEl.innerHTML = "00 : 00 : 00";
       }
-    }, 1000); // Run the timer every 1 second
+    }, 1000);
   }
-  
-  // Example usage
-//   var hours = parseInt(prompt("Enter the number of hours:"));
-//   var minutes = parseInt(prompt("Enter the number of minutes:"));
-  
-  startTimer(2, 30);
-  
+  startTimer(hours, minutes);
+}
