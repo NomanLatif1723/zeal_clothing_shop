eb923497dac064519b5b5a4a15a5881bae8c5ab1
NomanLatif1723 <nomanlatif1723@gmail.com>
2023-07-14 20:21:11 +0500

    timer js code

---
+++ assets/theme.js
@@ -0,0 +1,20 @@
+const startTime = 45;
+let time = startTime * 60 ;
+const timerEl = document.querySelector('.main__timer');
+
+
+let timer = setInterval(updateCountDown,1000);
+function updateCountDown(){
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  timerEl.innerHTML = `${minutes} : ${seconds}`;
+  if ( minutes == 0 && seconds < 1){
+    clearInterval(timer);
+    // document.querySelector('.timer_text_icon').style.display = 'none';
+    // document.querySelector('.timer_completion_msg').style.display = 'block';
+    
+  } else {
+  time--;
+  }
+}
