b4c96bd99c6564b1e7d855e814aae13184885214
NomanLatif1723 <nomanlatif1723@gmail.com>
2023-07-14 20:54:58 +0500

    initial commit

--- assets/theme.js
+++ assets/theme.js
@@ -1,34 +1,35 @@
 const timerEl = document.querySelector('.main__timer');
-const hours = timerEl.getAttribute('data-hour');
-const minutes = timerEl.getAttribute('data-minutes');
+
 if(timerEl){
-function startTimer(hours, minutes) {
-    // Convert hours and minutes to milliseconds
-    var totalMilliseconds = (hours * 3600000) + (minutes * 60000);
-  
-    // Ensure the timer is valid
-    if (totalMilliseconds <= 0) {
-      console.log("Invalid timer duration");
-      return;
+    const hours = timerEl.getAttribute('data-hour');
+    const minutes = timerEl.getAttribute('data-minutes');
+    function startTimer(hours, minutes) {
+        // Convert hours and minutes to milliseconds
+        var totalMilliseconds = (hours * 3600000) + (minutes * 60000);
+    
+        // Ensure the timer is valid
+        if (totalMilliseconds <= 0) {
+        console.log("Invalid timer duration");
+        return;
+        }
+    
+        var timerInterval = setInterval(function() {
+        // Calculate remaining hours, minutes, and seconds
+        var remainingHours = Math.floor(totalMilliseconds / 3600000);
+        var remainingMinutes = Math.floor((totalMilliseconds % 3600000) / 60000);
+        var remainingSeconds = Math.floor((totalMilliseconds % 60000) / 1000);
+        
+        timerEl.innerHTML = `${remainingHours} : ${remainingMinutes} : ${remainingSeconds}`;
+    
+        // Reduce remaining time by 1 second
+        totalMilliseconds -= 1000;
+    
+        // Check if the timer has ended
+        if (totalMilliseconds <= 0) {
+            clearInterval(timerInterval);
+            timerEl.innerHTML = "00 : 00 : 00";
+        }
+        }, 1000);
     }
-  
-    var timerInterval = setInterval(function() {
-      // Calculate remaining hours, minutes, and seconds
-      var remainingHours = Math.floor(totalMilliseconds / 3600000);
-      var remainingMinutes = Math.floor((totalMilliseconds % 3600000) / 60000);
-      var remainingSeconds = Math.floor((totalMilliseconds % 60000) / 1000);
-      
-      timerEl.innerHTML = `${remainingHours} : ${remainingMinutes} : ${remainingSeconds}`;
-  
-      // Reduce remaining time by 1 second
-      totalMilliseconds -= 1000;
-  
-      // Check if the timer has ended
-      if (totalMilliseconds <= 0) {
-        clearInterval(timerInterval);
-        timerEl.innerHTML = "00 : 00 : 00";
-      }
-    }, 1000);
-  }
-  startTimer(hours, minutes);
+    startTimer(hours, minutes);
 }
