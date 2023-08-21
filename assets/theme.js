30fdd8f73b2e0ff1ef213a15aadfed610dc47073
NomanLatif1723 <nomanlatif1723@gmail.com>
2023-08-21 12:35:59 +0500

    init

--- assets/theme.js
+++ assets/theme.js
@@ -455,6 +455,36 @@ function initComparison() {
 }
 initComparison();
 
+function initageVerificationPopUp() {
+  const cookieStorage = {
+    getItem: (item) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+        return cookies[item];
+    },
+    setItem: (item, value) => {
+        document.cookie = `${item}=${value};`
+    }
+}
+  const storageType  = localStorage;
+  const consentPropertyName = 'age-verification-consent';
+
+  const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+  const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+  window.onload = () => {
+    if (shouldShowPopup(storageType)) {
+      const consent = confirm('Agree to Our Terms And Conditions');
+      if (consent) {
+        saveToStorage(storageType);
+      }
+    }
+  }
+}
+initageVerificationPopUp();
+
 function initModalPopup() {
   let popupModal = document.querySelectorAll('[data-popup-modal]');
   popupModal.forEach(popup => {
