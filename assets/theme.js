67b8faac0085e8ee854cdb1fa655ba70f9d26fd9
NomanLatif1723 <nomanlatif1723@gmail.com>
2023-09-15 16:47:08 +0500

    product template

--- assets/theme.js
+++ assets/theme.js
@@ -554,32 +554,6 @@ function initModalPopup() {
   })
 }
 initModalPopup();
-
-// let localizationForm = document.querySelector('#localization_form');
-//   if (localizationForm) {
-//     let localeBtn = document.querySelectorAll('.disclosure__button');
-//     let localeLink = document.querySelectorAll('.disclosure__item a');
-//     let localeFormInput = document.querySelector('#localeId');
-//     let currencyFormInput = document.querySelector('#localeId');
-//     if (localeBtn) {
-//       localeBtn.forEach(button => {
-//         button.addEventListener('click', () => {
-//           button.closest('.disclosure').querySelector('.disclosure__list').toggleAttribute('hidden');
-//         })
-//       })
-//     }
-//     if (localeLink) {
-//       localeLink.forEach(button => {
-//         button.addEventListener('click', (event) => {
-//           event.preventDefault();
-//           localeFormInput.value = button.getAttribute('data-value');
-//           currencyFormInput.value = button.getAttribute('data-value');
-//           // localizationForm.submit();
-//         })
-//       })
-//     }
-    
-//   }
   
 // Localization Language and Currency Selector 
 class LocalizationForm extends HTMLElement {
@@ -998,5 +972,6 @@ function initcartAjax() {
   })
 }
 initcartAjax();
+
 
 })();
